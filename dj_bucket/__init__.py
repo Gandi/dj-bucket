4da3fbcc3acd07faa2048dc76740d9965c6dbb0c
Guillaume Gauvrit <guillaume.gauvrit@gandi.net>
2026-09-25 15:50:59 +0200

    Add a __version__

--- dj_bucket/__init__.py
+++ dj_bucket/__init__.py
@@ -1,1 +1,6 @@
 """A django application made to do django media storage using postgresql."""
+
+from importlib.metadata import version
+
+__version__ = version("dj-bucket")
+__all__ = ["__version__"]
